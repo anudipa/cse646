a23fd1b371468a744023f94491504c6743c3488c
Surya Kumar <ur2cdanger@gmail.com>
2013-05-05 02:02:55 -0400

    Files uploaded.upload working in background.Videorecord still working in front.Doing it

---
+++ SmartEye/src/edu/smarteye/sensing/VideoRecorder.java
@@ -0,0 +1,113 @@
+package edu.smarteye.sensing;
+
+//import com.example.project.R;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.Camera;
+import android.media.MediaRecorder;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+public class VideoRecorder extends Activity implements SurfaceHolder.Callback 
+{
+	String videofolder = android.os.Environment.getExternalStorageDirectory()+"/Record/";
+	private final String VIDEO_PATH_NAME = videofolder+"test.mp4";
+	private MediaRecorder mMediaRecorder;
+	private Camera mCamera;
+	private SurfaceView mSurfaceView;
+	private SurfaceHolder mHolder;
+	//private View mToggleButton;
+	private boolean mInitSuccesful;
+		
+	void startrecording(Context context)
+	{
+		mSurfaceView = new SurfaceView(context);
+	    mHolder = mSurfaceView.getHolder();
+	    mHolder.addCallback(this);
+	    mMediaRecorder.start();
+        try 
+        {
+            Thread.sleep(10 * 1000);
+        } 
+        catch (Exception e) 
+        {
+            e.printStackTrace();
+        }
+        //finish();
+        
+	}
+
+	private void initRecorder(Surface surface) throws IOException 
+	{
+	    
+	    if(mCamera == null) 
+	    {
+	        mCamera = Camera.open();
+	        mCamera.unlock();
+	    }
+	    if(mMediaRecorder == null) 
+	    	mMediaRecorder = new MediaRecorder();
+	    mMediaRecorder.setPreviewDisplay(surface);
+	    mMediaRecorder.setCamera(mCamera);
+	    mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.DEFAULT);
+	    mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+	    mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+	    mMediaRecorder.setVideoEncodingBitRate(512 * 1000);
+	    mMediaRecorder.setVideoFrameRate(30);
+	    mMediaRecorder.setVideoSize(640, 480);
+	    mMediaRecorder.setOutputFile(VIDEO_PATH_NAME);
+
+	    try 
+	    {
+	        mMediaRecorder.prepare();
+	    } 
+	    catch (IllegalStateException e)
+	    {
+	        e.printStackTrace();
+	    }
+
+	    mInitSuccesful = true;
+	}
+
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) 
+	{
+	    try 
+	    {
+	        if(!mInitSuccesful)
+	            initRecorder(mHolder.getSurface());
+	    } 
+	    catch (IOException e) 
+	    {
+	        e.printStackTrace();
+	    }
+	}
+
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder)
+	{
+	    shutdown();
+	}
+
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+	private void shutdown() 
+	{
+	    // Release MediaRecorder and especially the Camera as it's a shared
+	    // object that can be used by other applications
+		mMediaRecorder.stop();
+	    mMediaRecorder.reset();
+	    mMediaRecorder.release();
+	    mCamera.release();
+
+	    // once the objects have been released they can't be reused
+	    mMediaRecorder = null;
+	    mCamera = null;
+	}
+
+}
---
+++ SmartEye/src/edu/smarteye/sensing/videoupload.java
@@ -0,0 +1,70 @@
+package edu.smarteye.sensing;
+
+import java.io.FileInputStream;
+import java.net.InetAddress;
+import java.net.URL;
+
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class videoupload extends AsyncTask<URL,Integer,Long>
+{
+	String videofolder = android.os.Environment.getExternalStorageDirectory()+"/Record/";
+	private final String VIDEO_PATH_NAME = videofolder+"test.mp4";
+
+	@Override
+	protected Long doInBackground(URL... url) 
+	{
+		// TODO Auto-generated method stub
+		FTPClient ftp = new FTPClient();
+		  try
+		  {
+		      Log.v( "Connecting "," To server" );
+		      try
+		      {ftp.connect(InetAddress.getByName("ec2-50-17-179-124.compute-1.amazonaws.com"));}
+		      catch(Exception f)
+		      {
+		    	  f.printStackTrace();
+		    	  Log.v("E:",f.toString());
+		      }
+		      Log.v("Reply code : ",Integer.toString(ftp.getReplyCode()));
+		      if(!ftp.login("anonymous","anonymous"))
+		      {
+		          Log.v( "Login"," failed" );
+		          ftp.logout();
+		          
+		      }
+		      int reply = ftp.getReplyCode();
+		      Log.v( "Connect returned: " , "" );
+		      if (!FTPReply.isPositiveCompletion(reply)) 
+		      {
+		          ftp.disconnect();
+		          Log.v( "Connection "," failed" );
+		          
+		      }
+		      ftp.enterLocalPassiveMode(); 
+		      FileInputStream in = new FileInputStream(VIDEO_PATH_NAME);
+		      ftp.setFileType(ftp.BINARY_FILE_TYPE);
+		      Log.v( "Uploading"," File" );
+		      Log.v("Video ",VIDEO_PATH_NAME);
+		      Log.v("Dir ",ftp.printWorkingDirectory());
+		      ftp.changeWorkingDirectory("/uploads");
+		      Log.v("New Dir : ",ftp.printWorkingDirectory());
+		      boolean store = ftp.storeFile("/uploads",in); 
+		      Log.v("Store ",String.valueOf(store));
+		      in.close();
+		      ftp.logout();
+		      ftp.disconnect();
+		  }
+		  catch(Exception ex)
+		  {
+		      ex.printStackTrace();
+		      Log.v("Error: ",ex.toString());
+		  }
+		return null;
+	}
+	
+}
